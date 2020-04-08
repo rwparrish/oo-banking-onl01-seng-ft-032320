7ba3791b0f88cfea0b8d342172b1785fe069ba84
Ryan Parrish <rino3717@gmail.com>
2020-04-08 00:44:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :status, :amount, :BankAccount
+  attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiver, amount)
     @sender =  sender
