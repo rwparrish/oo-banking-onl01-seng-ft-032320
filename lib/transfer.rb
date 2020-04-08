a739e5aa3ebedec32a486e7d2ff5dd91d14a5f71
Ryan Parrish <rino3717@gmail.com>
2020-04-08 01:48:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,11 +17,12 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
-      @sender.balance > amount
-      @sender.balance - amount
-      @receiver.balance + amount
+    if valid? && @sender.balance > amount && @status == "pending"
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
     else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
