293f29698586008b0fd0eefd61fc0636d4054580
Ryan Parrish <rino3717@gmail.com>
2020-04-08 00:43:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
   
   def valid?
-    sender.BankAccount.valid?
-    receiver.BankAccount.valid?
+    sender.class.BankAccount.valid?
+    receiver.class.BankAccount.valid?
   end
 end
