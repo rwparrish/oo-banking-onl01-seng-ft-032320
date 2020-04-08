e34471c333fbbbc45129b90f6b8b85bc75bb4308
Ryan Parrish <rino3717@gmail.com>
2020-04-08 00:27:01 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,11 +6,15 @@ class BankAccount
 def initialize(name)
   @name = name
   @balance = 1000
-  @status = open
+  @status = "open"
 end
 
 def deposit(money)
-  @balance = balance + money
+  @balance = self + money
+end
+
+def display_balance
+  
 end
 
 end
