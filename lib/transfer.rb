15550cbaaba774610ac7c415f59c8a3fe1346a50
Ryan Parrish <rino3717@gmail.com>
2020-04-08 00:37:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,15 @@
 class Transfer
   
-  def initialize 
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sedner =  sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
     
   end
 end
